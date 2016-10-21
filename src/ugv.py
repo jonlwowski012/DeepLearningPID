43272109290eca29dbff40999b5cbb1455ca8154
jonlwowski012 <jonathan.lwowski@gmail.com>
2016-10-21 11:29:08 -0500

    added comments to all codes

--- src/ugv.py
+++ src/ugv.py
@@ -12,14 +12,16 @@ import rospy
 from geometry_msgs.msg import Twist 
 
 
-
-
-
 ### UGV Publisher Function ##### 
 
-def set_velocity_ugv(lx1, ly1, lz1, ax1, ay1, az1):   
+def set_velocity_ugv(lx1, ly1, lz1, ax1, ay1, az1):
+	### Setup Publisher to publish to /p3dx/cmd_vel with a Twist Message   
 	pub1 = rospy.Publisher('/p3dx/cmd_vel', Twist, queue_size=10)
-	r = rospy.Rate(50) # 50hz
+
+	### Publish messages at 50hz
+	r = rospy.Rate(50)
+
+	### Setup message to be published
 	command1 = Twist()
 	command1.linear.x = lx1
 	command1.linear.y = ly1
@@ -28,11 +30,11 @@ def set_velocity_ugv(lx1, ly1, lz1, ax1, ay1, az1):
 	command1.angular.y = ay1
 	command1.angular.z = az1
     
-   
+   	### Publish Message
 	pub1.publish(command1)
      
 
-
+### Main Function to make UGV drive in a circle.
 if __name__ == '__main__':
 	rospy.init_node('ugv', anonymous=True)
 	rospy.sleep(5.)
